d024c3d7ce1ceb5aa3f9e1e4e7d28dafb61a3d6a
ennilehtinen <ennilehtinen9@gmail.com>
2022-10-22 20:44:12 +0300

    osa4.2

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const middleware = require('./utils/middleware')
+
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
