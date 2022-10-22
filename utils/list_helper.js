7ba256f3732b9675d88382ba84ce3392af62787f
ennilehtinen <ennilehtinen9@gmail.com>
2022-10-22 21:30:29 +0300

    osa 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const totalLikes = blogs => {
   return blogs.reduce((accr, current) => accr + current.likes, 0)
 }
 
+const favoriteBlog = blogs => {
+  return blogs.reduce((accr, current) => {
+    return current.likes > (accr.likes ?? 0) ? current : accr
+  }, {})
+}
+
 module.exports = {
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
