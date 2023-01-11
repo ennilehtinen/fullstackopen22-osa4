e5db4482616c927de4c85989bb42cacf0d56e9dc
ennilehtinen <ennilehtinen9@gmail.com>
2023-01-11 15:50:44 +0200

    4.15

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,13 +1,17 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+  const blog = new Blog(body)
+
+  const user = await User.findById(body.userId)
 
   if (!blog.title || !blog.url) {
     response
@@ -20,7 +24,13 @@ blogsRouter.post('/', async (request, response) => {
     blog.likes = 0
   }
 
+  blog.user = user._id
+
   const newBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(newBlog._id)
+  await user.save()
+
   response.status(201).json(newBlog)
 })
 
