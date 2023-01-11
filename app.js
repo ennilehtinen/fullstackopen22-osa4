a875abfa96215491c0d3b77b7257491828e01e23
ennilehtinen <ennilehtinen9@gmail.com>
2023-01-11 16:21:18 +0200

    4.19

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const middleware = require('./utils/middleware')
+const config = require('./utils/config')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
@@ -21,6 +23,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
